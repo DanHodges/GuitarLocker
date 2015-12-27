87b40ece7f7cdf93a25f3423f8b50b262daf2839
DanHodges <dhdgs690@gmail.com>
2015-12-27 14:26:17 -0600

    deleted wilder

--- src/Wildermuth/wwwroot/lib/jquery-validation/src/additional/strippedminlength.js
+++
@@ -1,4 +0,0 @@
-// TODO check if value starts with <, otherwise don't try sInstrumentping anything
-$.validator.addMethod("sInstrumentpedminlength", function(value, element, param) {
-	return $(value).text().length >= param;
-}, $.validator.format("Please enter at least {0} characters"));
